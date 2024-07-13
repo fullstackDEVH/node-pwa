d6d0f37ed8b5db21da5b37813ba46423e40f2301
fullstackDEVH <huagiahuy20052001@gmail.com>
2024-07-14 01:50:47 +0700

    change folder to deloy vercel

--- a.js
+++
@@ -1,7 +0,0 @@
-const webpush = require('web-push');
-
-// Tạo các khóa VAPID
-const vapidKeys = webpush.generateVAPIDKeys();
-
-console.log('VAPID Public Key:', vapidKeys.publicKey);
-console.log('VAPID Private Key:', vapidKeys.privateKey);
